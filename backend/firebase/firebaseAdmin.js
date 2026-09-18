74f2b7ada31d8479f6ecfd4f782e39fc83f20432
unknown <oyinolaosebikan10@gmail.com>
2026-09-18 13:54:50 +0100

    Prepare backend for Render deployment

--- backend/firebase/firebaseAdmin.js
+++ backend/firebase/firebaseAdmin.js
@@ -1,11 +1,27 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
-
+const fs = require("fs");
 const path = require("path");
 
-const serviceAccount = require(
-    path.join(__dirname, "..", "firebase-service-account.json")
+let serviceAccountPath;
+
+// Render Secret Files are available in /etc/secrets
+const renderSecretPath = "/etc/secrets/firebase-service-account.json";
+
+// Local development file
+const localSecretPath = path.join(
+    __dirname,
+    "..",
+    "firebase-service-account.json"
 );
+
+if (fs.existsSync(renderSecretPath)) {
+    serviceAccountPath = renderSecretPath;
+} else {
+    serviceAccountPath = localSecretPath;
+}
+
+const serviceAccount = require(serviceAccountPath);
 
 if (!getApps().length) {
     initializeApp({
